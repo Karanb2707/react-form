78a27ec70a0b22b1cc3e281e8711cfde5f376006
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-05-22 01:15:56 +0530

    continu

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
 
 
 function App() {
 
+  // Single state handler
+
+  // const [firstName, setFirstName] = useState('');
+  // const [lastName, setLastName] = useState('');
+
+  // function firstNameHanlder(event) {
+  //   setFirstName(event.target.value);
+  //   console.log(firstName);
+  // }
+
+  // function lastNameHandler(event) {
+  //   setLastName(event.target.value);
+  //   console.log(lastName);
+  // }
+
+  // Multiple state handler
+  // 21 min
+
   return (
-    <div className="text-center">App</div>
+    <div className="text-center mt-2">
+      <form action="">
+        <input type="text" placeholder="First name" onChange={firstNameHanlder} className="border p-1"/>
+        <br /> <br />
+        <input type="text" placeholder="Last name" onChange={lastNameHandler}  className="border p-1"/>
+      </form>
+    </div>
   )
 }
 
